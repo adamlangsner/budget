91b5f1fad41c2f38008d870474fa1aeda29ac1d1
Adam Langsner <adamlangsner@gmail.com>
2013-11-05 20:38:04 -0500

    added comments

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -12,12 +12,15 @@ define(
 function ($, _, Backbone, Marionette, GraphInfo, GraphView, SideBarView) {
 
     App = new Marionette.Application();
-    
+
     App.addInitializer(function(options) {
-        _.extend(App, {});
+        _.extend(App, {
+            // add methods to App object here
+        });
     });
 
     App.addInitializer(function(options) {
+        // create application level regions for sidebar and graph
         App.addRegions({
             sideBar: "section#sideBar",
             graphArea: "section#graphArea"
@@ -27,13 +30,17 @@ function ($, _, Backbone, Marionette, GraphInfo, GraphView, SideBarView) {
     // gets triggered when app starts
     App.on("start", function() {
         var now = moment().startOf('day');
+
+        // create a graphInfo model and hang it off of App object
         App.graphInfo = new GraphInfo({
             start: now,
             end: now.clone().add('months', 12)
         });
 
+        // download the budget from the server
         App.graphInfo.fetch({
             success: function() {
+                // show the views in their respective regions with the data we just downloaded
                 App.sideBar.show(new SideBarView({ model: App.graphInfo }));
                 App.graphArea.show(new GraphView({ model: App.graphInfo }));
             }
--- server.js
+++ server.js
@@ -5,12 +5,21 @@ var _        = require("underscore"),
     app      = express(),
     Schema   = mongoose.Schema;
 
+// connect to mongo database
 console.log('connecting to mongoDB...');
 var db = mongoose.connect('mongodb://localhost/budget').connection;
+
+// error connecting to mongo
 db.on('error', console.error.bind(console, 'connection error:'));
+
+// successful connection to mongo
 db.once('open', function () {
+
+  // create mongoose model
   var Budget = mongoose.model('Budget', new Schema({
     currentBalance: Number,
+
+    // each budget has an array of transactions objects
     transactions: [new Schema({
       name: String,
       amount: Number,
@@ -22,44 +31,54 @@ db.once('open', function () {
   }));
 
   app.configure(function() {
+    // set ejs as view rendering engine
     app.set('view engine', 'ejs');
     app.set('views', __dirname+'/views');
 
     app.use(express.methodOverride());
     app.use(express.bodyParser());
 
+    // serves frontend application
     app.use(express.static('public'));
+
     app.use(express.logger());
     app.use(app.router);
 
     app.use(express.errorHandler({
-      dumpExceptions: true, 
+      dumpExceptions: true,
       showStack: true
     }));
   });
 
+  // serves index ejs file
   app.get("/", function(req, res) {
     res.render('index');
   });
 
+  // endpoint to get budget
   app.get("/api/budget", function(req, res) {
+    // for now, get the first budget in the db
     Budget.findOne({}, function(err, budget) {
       res.end(JSON.stringify(budget.toJSON()));
     });
   });
 
+  // endpoint to create a new budget
   app.post("/api/budget", function(req, res) {
     new Budget(req.body).save(function() {
       res.end();
     });
   });
 
+  // endpoint to update a budget
   app.put("/api/budget", function(req, res) {
+    // currently updates all budgets in the collection to the new budget
     Budget.update({}, {$set: req.body}, {upsert: true}, function(err) {
       res.end();
     });
   });
 
+  // start express server
   console.log('starting express server...');
   http.createServer(app).listen(4000);
 });
